321427c7cc61ee4986be2955c6e3f2d0ba629135
Taufiq Pohan <m.taufiq30s@hotmail.com>
2024-03-30 15:58:04 +0700

    main: Implement env for port
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- cmd/adzan/main.go
+++ cmd/adzan/main.go
@@ -2,19 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/taufiq30s/adzan/internal/routes"
 )
 
 func main() {
+	defer catch()
+
 	router := routes.NewRoute()
 
-	port := 8080
-	addr := fmt.Sprintf(":%d", port)
-	fmt.Printf("Server listening on http://localhost%s\n", addr)
-	err := http.ListenAndServe(addr, router)
+	port, err := getPort()
 	if err != nil {
 		panic(err)
+	}
+
+	addr := fmt.Sprintf(":%s", port)
+	fmt.Printf("Server listening on http://localhost%s\n", addr)
+	err = http.ListenAndServe(addr, router)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func getPort() (string, error) {
+	if err := godotenv.Load(".env"); err != nil {
+		return "", err
+	}
+	return os.Getenv("PORT"), nil
+}
+
+func catch() {
+	if r := recover(); r != nil {
+		log.Printf("panic: %v", r)
 	}
 }
